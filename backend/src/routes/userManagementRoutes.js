354f0f7b976407951cc0aad5b694cb5cc0a649af
Andrew Igure <andrewoigure@gmail.com>
2025-10-16 23:19:52 +0100

    Fix: Change requireRole to verifyRole in userManagementRoutes

--- backend/src/routes/userManagementRoutes.js
+++ backend/src/routes/userManagementRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateToken } = require('../middlewares/authMiddleware');
-const { requireRole } = require('../middlewares/roleMiddleware');
+const { verifyRole } = require('../middlewares/roleMiddleware');
 const {
   lockUser,
   unlockUser,
@@ -19,28 +19,28 @@ const {
  */
 
 // Lock user account
-router.put('/:id/lock', authenticateToken, requireRole(['MasterAdmin']), lockUser);
+router.put('/:id/lock', authenticateToken, verifyRole(['MasterAdmin']), lockUser);
 
 // Unlock user account
-router.put('/:id/unlock', authenticateToken, requireRole(['MasterAdmin']), unlockUser);
+router.put('/:id/unlock', authenticateToken, verifyRole(['MasterAdmin']), unlockUser);
 
 // Soft delete user account (preserve data)
-router.delete('/:id/soft', authenticateToken, requireRole(['MasterAdmin']), softDeleteUser);
+router.delete('/:id/soft', authenticateToken, verifyRole(['MasterAdmin']), softDeleteUser);
 
 // Hard delete user account (permanent deletion)
-router.delete('/:id/hard', authenticateToken, requireRole(['MasterAdmin']), hardDeleteUser);
+router.delete('/:id/hard', authenticateToken, verifyRole(['MasterAdmin']), hardDeleteUser);
 
 // Restore soft-deleted user account
-router.put('/:id/restore', authenticateToken, requireRole(['MasterAdmin']), restoreUser);
+router.put('/:id/restore', authenticateToken, verifyRole(['MasterAdmin']), restoreUser);
 
 // Get all soft-deleted users
-router.get('/deleted', authenticateToken, requireRole(['MasterAdmin']), getDeletedUsers);
+router.get('/deleted', authenticateToken, verifyRole(['MasterAdmin']), getDeletedUsers);
 
 // Get user activity history (for soft-deleted users)
-router.get('/:id/activity', authenticateToken, requireRole(['MasterAdmin']), getUserActivity);
+router.get('/:id/activity', authenticateToken, verifyRole(['MasterAdmin']), getUserActivity);
 
 // Check user status (locked/deleted)
-router.get('/:id/status', authenticateToken, requireRole(['MasterAdmin']), checkUserStatus);
+router.get('/:id/status', authenticateToken, verifyRole(['MasterAdmin']), checkUserStatus);
 
 module.exports = router;
 
